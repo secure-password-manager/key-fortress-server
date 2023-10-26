2e62c63156309167c0c43e8ea274c07c6d6043bd
Angie Yeh <angieyeh24@gmail.com>
2023-10-25 18:46:39 -0700

    Create settings modules to create separate local and production config files

--- config/wsgi.py
+++ config/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv, find_dotenv
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+load_dotenv(find_dotenv())
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.environ['DJANGO_SETTINGS_MODULE'])
 
 application = get_wsgi_application()
