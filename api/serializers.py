e82fb154a7b67453a4b81003ae1c9bff02164a8e
Angie Yeh <angieyeh24@gmail.com>
2023-11-03 15:07:11 -0700

    Create POST request handler and tests for creating vault items

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,10 @@
 from django.contrib.auth import authenticate
+from django.shortcuts import get_object_or_404
 
 from rest_framework.exceptions import AuthenticationFailed, ValidationError
-from rest_framework.serializers import Serializer, CharField, EmailField
+from rest_framework.serializers import Serializer, CharField, EmailField, UUIDField
+
+from api.models import VaultItem, VaultCollection
 
 
 class LoginSerializer(Serializer):
@@ -20,3 +23,30 @@ class LoginSerializer(Serializer):
 
         data['user'] = user
         return data
+
+
+class VaultItemSerializer(Serializer):
+
+    encrypted_data = CharField(required=True)
+    vault_collection_uuid = UUIDField(required=True)
+
+    def create(self, validated_data):
+        return VaultItem.objects.create(**validated_data)
+
+    def validate(self, data):
+        current_user = self.context['request'].user
+        encrypted_data = data.get('encrypted_data')
+        vault_collection_uuid = data.get('vault_collection_uuid')
+
+        if not (encrypted_data and vault_collection_uuid):
+            raise ValidationError()
+        vault_collection = get_object_or_404(
+            VaultCollection,
+            user_id=current_user.id,
+            uuid=vault_collection_uuid
+        )
+
+        return {
+            'encrypted_data': data.get('encrypted_data'),
+            'vault_collection_id': vault_collection.id
+        }
--- api/views.py
+++ api/views.py
@@ -5,7 +5,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .serializers import LoginSerializer
+from .serializers import LoginSerializer, VaultItemSerializer
 
 
 class LoginAPIView(APIView):
@@ -34,3 +34,12 @@ class TestView(APIView):
     def get(self, request): # noqa
         return Response(data={'detail': 'Congratulations! You are an authenticated user!'},
                         status=status.HTTP_200_OK)
+
+
+class VaultItemAPIView(APIView):
+    def post(self, request):
+        serializer = VaultItemSerializer(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(status=status.HTTP_200_OK)
