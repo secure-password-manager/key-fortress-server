83092d76006197477bc539db89af590e3df40e14
Derth Adams <derth@me.com>
2023-11-07 13:26:25 -0800

    Configure CORS, CSRF and SESSION_COOKIE settings to allow cross-origin authentication

--- config/settings/local.py
+++ config/settings/local.py
@@ -3,3 +3,12 @@ from .base import *
 ALLOWED_HOSTS = ['localhost']
 
 DEBUG = True
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173'
+]
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173'
+]
