7d16a0850edb9c5fb04708be55f431c4deca651d
Angie Yeh <angieyeh24@gmail.com>
2023-11-11 17:07:19 -0800

    Add tests for VaultCollection GET

--- api/tests/test_vault_collection.py
+++ api/tests/test_vault_collection.py
@@ -62,3 +62,50 @@ class TestCreateVaultCollectionViewSet(APITestCase):
         vault_collection = VaultCollection.objects.filter(
             user=self.user)
         self.assertEqual(len(vault_collection), 0)
+
+
+class TestGetVaultCollectionViewSet(APITestCase):
+
+    def setUp(self):
+        self.vault_collections_url = reverse('vault_collection-list')
+
+    def test_vault_collections_get_success(self):
+        user_data = {
+            'email': 'pippa1@gmail.com',
+            'password': 'super-password'
+        }
+        user = get_user_model().objects.create_user(**user_data)
+
+        vc = VaultCollection.objects.create(
+            name='folder1', user_id=user.id)
+        VaultItem.objects.create(
+            encrypted_data='encrypted data 1', vault_collection_id=vc.id)
+        VaultItem.objects.create(
+            encrypted_data='encrypted data 2', vault_collection_id=vc.id)
+        self.client.login(email='pippa1@gmail.com', password='super-password')
+        response = self.client.get(self.vault_collections_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(len(response.data[0]['vault_items']), 2)
+
+        user_data = {
+            'email': 'pippa2@gmail.com',
+            'password': 'super-password'
+        }
+        user = get_user_model().objects.create_user(**user_data)
+        VaultCollection.objects.create(
+            name='folder1', user_id=user.id)
+        VaultCollection.objects.create(
+            name='folder2', user_id=user.id)
+        self.client.login(email='pippa2@gmail.com', password='super-password')
+        response = self.client.get(self.vault_collections_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 2)
+
+    def test_vault_collections_get_user_not_authenticated(self):
+        self.client.login(email='pippa1@gmail.com', password='super-password')
+        self.client.logout()
+        response = self.client.get(self.vault_collections_url)
+        self.assertEqual(response.status_code, 403)
+        self.assertIn(
+            'Authentication credentials were not provided.', response.data['detail'])
